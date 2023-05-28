86fb501a467e0f35740457e6ceb4dbbfabc80b59
Damien Bezborodov <dbezborodov@gmail.com>
2023-05-28 13:41:59 +0930

    Implement elementLoaded.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 function elementLoaded(selector) {
+  return new Promise((resolve, reject) => {
+    const target = document;
 
+    const firstPass = target.querySelector(selector);
+    if (firstPass) {
+      resolve(firstPass);
+      return;
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        if (mutation.type == 'childList') {
+          for (const addedNode of mutation.addedNodes) {
+            if (addedNode instanceof HTMLElement) {
+              if (addedNode.matches(selector)) {
+                observer.disconnect();
+                resolve(addedNode);
+                return;
+              }
+            }
+          }
+        }
+      }
+    });
+    document.addEventListener('DOMContentLoaded', () => {
+      reject('DOM finished loading, not found.');
+      observer.disconnect();
+    });
+    if (document.readyState != 'loading') {
+      reject('DOM already loaded, not found.');
+      return;
+    }
+    observer.observe(target, { subtree: true, childList: true });
+  });
 }
